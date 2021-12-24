87dbbdb76979449a5499eea88bb5a42f8cd32970
riboney <14286113+Ribby-0254@users.noreply.github.com>
2021-12-24 00:00:06 -0500

    Finishes day 5; submits a project from udemy tutorial

---
+++ day-5/scripts.js
@@ -0,0 +1,8 @@
+let panels = document.querySelectorAll('.panel');
+
+panels.forEach(panel => {
+  panel.addEventListener('click', (event) => {
+      panels.forEach(panel => panel.classList.remove('active'));
+      panel.classList.add('active');
+  })  
+});
